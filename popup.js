990ab3e72bf8e9e5b6f0e184c96280e26e3ca072
Bryan Mytko <bryanmytko@gmail.com>
2013-11-26 21:45:11 -0500

    fixed console log

--- popup.js
+++ popup.js
@@ -5,7 +5,6 @@ $('#sell_div').append('<input type="text" id="cash" placeholder="How much cash?"
 
 $('#sell_div').append('<input type="text" id="meth" placeholder="How much meth?"/><button onclick="gm.add_widgets(parseInt($(\'#meth\').val()))" id="get_meth">Gimme Meth!</button>');
 
-console.log(chrome.extension.getURL('hector.png'));
 i = chrome.extension.getURL('hector.png');
 
 $('#sell_div').css({'background' : 'url(' + i + ')', 'padding':'0 0 96px 0'});
